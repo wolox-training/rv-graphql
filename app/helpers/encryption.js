1eb58667aa89284d036b0d7b044d65cecfe4f77e
Rodrigo <rvidela@outlook.com>
2019-07-30 17:12:20 -0300

    The encryption process is now asynchronous.

--- app/helpers/encryption.js
+++ app/helpers/encryption.js
@@ -1,11 +1,19 @@
+/* eslint-disable curly */
 const bcrypt = require('bcryptjs');
 
 const saltValue = 10;
 const salt = bcrypt.genSaltSync(saltValue);
 
 const encryptPassword = password => bcrypt.hashSync(password, salt);
+const encryptPasswordAsync = password =>
+  new Promise((resolve, reject) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  });
 
 const checkPassword = (plaintextPassword, hashedPassword) =>
   bcrypt.compareSync(plaintextPassword, hashedPassword);
 
-module.exports = { encryptPassword, checkPassword };
+module.exports = { encryptPassword, checkPassword, encryptPasswordAsync };
--- app/services/users.js
+++ app/services/users.js
@@ -1,18 +1,19 @@
 /* eslint-disable curly */
-const { encryptPassword } = require('../helpers/encryption');
+const { encryptPasswordAsync } = require('../helpers/encryption');
 const logger = require('../logger/index');
 const { user: User } = require('../models');
 
 const createUserMethod = async (_, { user }) => {
   try {
-    user.password = encryptPassword(user.password);
+    const encryptedPassword = await encryptPasswordAsync(user.password);
+    user.password = encryptedPassword;
     const createdUser = await User.createModel(user);
     logger.info(
       `The user ${user.firstName} ${user.lastName} was successfully created ${JSON.stringify(createdUser)}`
     );
     return createdUser;
   } catch (error) {
-    logger.error(error.name)
+    logger.error(error.name);
     error.errors.forEach(element => {
       logger.error(element.message);
     });
