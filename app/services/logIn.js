c27b23bc8742d4a1c27f9b65690368b2b4f3cf98
Rodrigo <rvidela@outlook.com>
2019-08-06 14:25:00 -0300

    The error handling of the signIn was not done right, now it is.

--- app/services/logIn.js
+++ app/services/logIn.js
@@ -1,23 +1,30 @@
 /* eslint-disable curly */
 const { user: User } = require('../models');
 const { checkPassword } = require('../helpers/encryption');
-const { unauthorized, badRequest } = require('../errors');
+const { unauthorized } = require('../errors');
 const { signToken } = require('../helpers/token');
 
 const areCredentialsPresent = user => {
-  if (!user.username) return badRequest('No input username!');
-  if (!user.password) return badRequest('No input password!');
-  return 0;
+  const errors = [];
+  if (!user.username) errors.push('No input username!');
+  if (!user.password) errors.push('No input password!');
+  return errors;
 };
 
 const isLoginValid = async user => {
   const { username, password } = user;
   const obtainedUser = await User.getByUsername(username);
+  const errors = [];
 
-  if (!obtainedUser) return unauthorized(`The username: ${username} is not registered.`);
+  if (!obtainedUser) {
+    errors.push(`The username: ${username} is not registered.`);
+    return errors;
+  }
+
   if (!checkPassword(password, obtainedUser.dataValues.password))
-    return unauthorized(`The password for the user with the username: ${username} was wrong.`);
-  return 0;
+    errors.push(`The password for the user with the username: ${username} was wrong.`);
+
+  return errors;
 };
 
 const signIn = user => {
--- app/services/signInQuery.js
+++ app/services/signInQuery.js
@@ -1,13 +1,18 @@
+/* eslint-disable curly */
 const { areCredentialsPresent, isLoginValid, signIn } = require('../services/logIn');
 const { JWT_EXPIRATION_TIME } = require('../../config/environment');
-const { internalServerError } = require('../errors');
+const { internalServerError, badRequest, unauthorized } = require('../errors');
 const logger = require('../logger/index');
 const { userLoggedIn } = require('../graphql/events');
 
 const signInQuery = async credentials => {
   try {
-    areCredentialsPresent(credentials);
-    await isLoginValid(credentials);
+    const presentCredentialsErrors = areCredentialsPresent(credentials);
+    if (presentCredentialsErrors.length) return badRequest(presentCredentialsErrors);
+
+    const validLoginErrors = await isLoginValid(credentials);
+    if (validLoginErrors.length) return unauthorized(validLoginErrors);
+
     userLoggedIn.publish(credentials.username);
     return {
       accessToken: signIn(credentials),
